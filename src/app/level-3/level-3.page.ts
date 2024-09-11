62b1e88289ebe6acd455543e8c31410235f7a3c4
krtRthr <140688186+krtRthr@users.noreply.github.com>
2024-09-11 14:41:08 +0800

    hjghj

--- src/app/level-3/level-3.page.ts
+++ src/app/level-3/level-3.page.ts
@@ -5,29 +5,29 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './level-3.page.html',
   styleUrls: ['./level-3.page.scss'],
 })
-export class Level3Page  { 
+export class Level3Page { 
 
- imageSrc1 = '../../assets/AlgebraQuest_Asset/lvl-buttons//lvl3-buttons/Group 49 (1).png';
- imageSrc2 = '../../assets/AlgebraQuest_Asset/lvl-buttons//lvl3-buttons/Group 50 (1).png';
- imageSrc3 = '../../assets/AlgebraQuest_Asset/lvl-buttons//lvl3-buttons/Group 51 (1).png';
- imageSrc4 = '../../assets/AlgebraQuest_Asset/lvl-buttons//lvl3-buttons/Group 52 (1).png';
- imageSrc5 = '../../assets/AlgebraQuest_Asset/lvl-buttons//lvl3-buttons/Group 53 (1).png';
- imageSrc6 = '../../assets/AlgebraQuest_Asset/lvl-buttons//lvl3-buttons/Group 54 (1).png';
- imageSrc7 = '../../assets/AlgebraQuest_Asset/lvl-buttons//lvl3-buttons/Group 55 (1).png';
- imageSrc8 = '../../assets/AlgebraQuest_Asset/lvl-buttons//lvl3-buttons/Group 56 (1).png';
- imageSrc9 = '../../assets/AlgebraQuest_Asset/lvl-buttons//lvl3-buttons/Group 57 (1).png';
- imageSrc10 = '../../assets/AlgebraQuest_Asset/lvl-buttons//lvl3-buttons/Group 58 (1).png';
+ imageSrc1 = '../../assets/AlgebraQuest_Asset/lvl-buttons/lvl3-buttons/on-level1.png';
+ imageSrc2 = '../../assets/AlgebraQuest_Asset/lvl-buttons/lvl3-buttons/close-level2.png';
+ imageSrc3 = '../../assets/AlgebraQuest_Asset/lvl-buttons/lvl3-buttons/close-level3.png';
+ imageSrc4 = '../../assets/AlgebraQuest_Asset/lvl-buttons/lvl3-buttons/close-level4.png';
+ imageSrc5 = '../../assets/AlgebraQuest_Asset/lvl-buttons/lvl3-buttons/close-level5.png';
+ imageSrc6 = '../../assets/AlgebraQuest_Asset/lvl-buttons/lvl3-buttons/close-level6.png';
+ imageSrc7 = '../../assets/AlgebraQuest_Asset/lvl-buttons/lvl3-buttons/close-level7.png';
+ imageSrc8 = '../../assets/AlgebraQuest_Asset/lvl-buttons/lvl3-buttons/close-level8.png';
+ imageSrc9 = '../../assets/AlgebraQuest_Asset/lvl-buttons/lvl3-buttons/close-level9.png';
+ imageSrc10 = '../../assets/AlgebraQuest_Asset/lvl-buttons/lvl3-buttons/close-level10.png';
 
- newImageSrc1 = '../../assets/AlgebraQuest_Asset/lvl-buttons//lvl3-buttons/Group 49 (1).png';
- newImageSrc2 = '../../assets/AlgebraQuest_Asset/lvl-buttons//lvl3-buttons/Group 39 (1).png';
- newImageSrc3 = '../../assets/AlgebraQuest_Asset/lvl-buttons//lvl3-buttons/Group 40 (1).png';
- newImageSrc4 = '../../assets/AlgebraQuest_Asset/lvl-buttons//lvl3-buttons/Group 41 (1).png';
- newImageSrc5 = '../../assets/AlgebraQuest_Asset/lvl-buttons//lvl3-buttons/Group 42 (1).png';
- newImageSrc6 = '../../assets/AlgebraQuest_Asset/lvl-buttons//lvl3-buttons/Group 48 (1).png';
- newImageSrc7 = '../../assets/AlgebraQuest_Asset/lvl-buttons//lvl3-buttons/Group 44 (1).png';
- newImageSrc8 = '../../assets/AlgebraQuest_Asset/lvl-buttons//lvl3-buttons/Group 45 (1).png';
- newImageSrc9 = '../../assets/AlgebraQuest_Asset/lvl-buttons//lvl3-buttons/Group 46 (1).png';
- newImageSrc10 = '../../assets/AlgebraQuest_Asset/lvl-buttons//lvl3-buttons/Group 47 (1).png';
+ newImageSrc1 = '../../assets/AlgebraQuest_Asset/lvl-buttons/lvl3-buttons/on-level1.png';
+ newImageSrc2 = '../../assets/AlgebraQuest_Asset/lvl-buttons/lvl3-buttons/on-level2.png';
+ newImageSrc3 = '../../assets/AlgebraQuest_Asset/lvl-buttons/lvl3-buttons/on-level3.png';
+ newImageSrc4 = '../../assets/AlgebraQuest_Asset/lvl-buttons/lvl3-buttons/on-level4.png';
+ newImageSrc5 = '../../assets/AlgebraQuest_Asset/lvl-buttons/lvl3-buttons/on-level5.png';
+ newImageSrc6 = '../../assets/AlgebraQuest_Asset/lvl-buttons/lvl3-buttons/on-level6.png';
+ newImageSrc7 = '../../assets/AlgebraQuest_Asset/lvl-buttons/lvl3-buttons/on-level7.png';
+ newImageSrc8 = '../../assets/AlgebraQuest_Asset/lvl-buttons/lvl3-buttons/on-level8.png';
+ newImageSrc9 = '../../assets/AlgebraQuest_Asset/lvl-buttons/lvl3-buttons/on-level9.png';
+ newImageSrc10 = '../../assets/AlgebraQuest_Asset/lvl-buttons/lvl3-buttons/on-level10.png';
   
 
 changeImage(event: Event, buttonId: number){
