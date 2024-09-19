5968c71a265b334027f1085ca44650ff587b09c5
krtRthr <140688186+krtRthr@users.noreply.github.com>
2024-09-19 22:42:33 +0800

    changed image in level cards
    pinalitan ko lang yung image ng parang naka unlock pa yung level3 at 2 na card

--- src/app/landing-page/landing-page.page.ts
+++ src/app/landing-page/landing-page.page.ts
@@ -5,9 +5,28 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './landing-page.page.html',
   styleUrls: ['./landing-page.page.scss'],
 })
-export class LandingPagePage implements OnInit {
+export class LandingPagePage {
 
-  constructor() { }
+  /*
+  imageSrc1 = '../../assets/AlgebraQuest_Asset/level 2 card unlocked.png';
+  imageSrc2 = '../../assets/AlgebraQuest_Asset/level 3 card unlocked.png';
+
+  newImageSrc1 = '../../assets/AlgebraQuest_Asset/level 2 card (1).png';
+  newImageSrc2 = '../../assets/AlgebraQuest_Asset/LEVEL 3 CARD (1).png';
+
+  changeImage(event: Event, buttonId: number) {  
+    const target = event.target as HTMLImageElement;
+    switch (buttonId) {
+      case 1:
+        target.src = this.newImageSrc1;
+        break;
+      case 2:
+        target.src = this.newImageSrc2;
+        break;
+      default:
+        break;
+    }
+  }*/
 
   playButton(){
     let audio = new Audio();
@@ -16,7 +35,4 @@ export class LandingPagePage implements OnInit {
     audio.play();
    }  
   
-   
-  ngOnInit() {
-  }
 }
