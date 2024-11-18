0c43d1325ea5acb6796dd3f8e0e129edc0df47c9
krtRthr <140688186+krtRthr@users.noreply.github.com>
2024-11-18 08:09:50 +0800

    damn tapos na pre sheeshh

--- src/app/level1-questions/question4/question4.page.ts
+++ src/app/level1-questions/question4/question4.page.ts
@@ -10,7 +10,7 @@ import { ScoreService } from 'src/app/score.service';
 })
 export class Question4Page {
 
-  correctAnswer: string = 'choice-2';
+  correctAnswer: string = 'choice-1';
   selectedAnswer: string = ''; 
 
   constructor(
